78ff9b03cf3236b4a93192f03a0b05c419a22f5b
Jmevorach <jacob.t.mevorach@gmail.com>
2025-07-31 10:04:20 -0400

    updates

--- openemr_ecs/openemr_ecs_stack.py
+++ openemr_ecs/openemr_ecs_stack.py
@@ -48,8 +48,8 @@ class OpenemrEcsStack(Stack):
         self.valkey_port = 6379
         self.container_port = 443
         self.number_of_days_to_regenerate_ssl_materials = 2
-        self.emr_serverless_release_label = "emr-7.8.0"
-        self.aurora_mysql_engine_version = rds.AuroraMysqlEngineVersion.VER_3_08_1
+        self.emr_serverless_release_label = "emr-7.9.0"
+        self.aurora_mysql_engine_version = rds.AuroraMysqlEngineVersion.VER_3_09_0
         self.openemr_version = "7.0.3"
         self.lambda_python_runtime = _lambda.Runtime.PYTHON_3_13
 
